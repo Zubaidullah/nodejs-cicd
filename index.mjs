8f49e72c8d67e2d851ba8ba364fbb83e60cc3c3e
Zubaidullah <zubair.ahmad@mycit.ie>
2026-02-15 10:29:57 +0000

    delete /about

--- index.mjs
+++ index.mjs
@@ -6,9 +6,6 @@ app.get("/", (req, res) => {
   res.send("From the index.mjs file");
 });
 
-app.get("/about", (req, res) => {
-  res.send("making some changes to the about page more changes to the about page");
-});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
