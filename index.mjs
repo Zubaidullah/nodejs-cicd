0d66914efc7baa663c9b5a95acd6734d2770fc21
Zubaidullah <zubair.ahmad@mycit.ie>
2026-02-15 10:01:53 +0000

    add a new route /about

--- index.mjs
+++ index.mjs
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("From the index.mjs file");
 });
 
+app.get("/about", (req, res) => {
+  res.send("This is the about page");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
